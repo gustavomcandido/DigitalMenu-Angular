89d1c506707f4c282bc71b4f5ee9b05e1770142d
gustavomcandido <gustavo.candido@mc1.com.br>
2020-06-09 21:41:20 -0300

    module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
+import { ReadComponent } from './admin/main-content/shared/read/read.component';
 
 
 const routes: Routes = [
-  {path: '', component: AdminComponent},
-  {path: 'login', component:LoginComponent}
+  {path: 'login', component:LoginComponent},
+  {
+    path: '', component: AdminComponent,
+    children: [
+      {path: '', component: ReadComponent}
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './admin/shared/sidebar/sidebar.component';
 import { MenuComponent } from './admin/shared/menu/menu.component';
 import { MenuItemComponent } from './admin/shared/menu.item/menu.item.component';
+import { MainContentComponent } from './admin/main-content/main-content.component';
+import { TableListComponent } from './core/table/table-list/table-list.component';
+import { TableListItemComponent } from './core/table/table-list/shared/table-list-item/table-list-item.component';
+import { ReadComponent } from './admin/main-content/shared/read/read.component';
+import { CreateComponent } from './admin/main-content/shared/create/create.component';
+import { UpdateComponent } from './admin/main-content/shared/update/update.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,11 +23,18 @@ import { MenuItemComponent } from './admin/shared/menu.item/menu.item.component'
     LoginComponent,
     SidebarComponent,
     MenuComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    MainContentComponent,
+    TableListComponent,
+    TableListItemComponent,
+    ReadComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
